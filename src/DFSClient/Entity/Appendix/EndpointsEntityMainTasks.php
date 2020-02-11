974a1a77167da0054861b2d8be4d31a44de197f7
Sergey <skovorodkinsergey86@gmail.com>
2020-02-11 16:07:04 +0200

    endpoints model

---
+++ src/DFSClient/Entity/Appendix/EndpointsEntityMainTasks.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+use DFSClientV3\Entity\Custom\EndpointsEntityMainTasksData;
+
+class EndpointsEntityMainTasks 
+{    
+    /**
+    * @var null|string $id;
+    */
+    public $id = null;
+
+    /**
+    * @var null|integer $status_code;
+    */
+    public $status_code = null;
+
+    /**
+    * @var null|string $status_message;
+    */
+    public $status_message = null;
+
+    /**
+    * @var null|string $time;
+    */
+    public $time = null;
+
+    /**
+    * @var null|integer $cost;
+    */
+    public $cost = null;
+
+    /**
+    * @var null|integer $result_count;
+    */
+    public $result_count = null;
+
+    /**
+    * @var null|array $path;
+    */
+    public $path = null;
+
+    /**
+    * @var null|EndpointsEntityMainTasksData $data;
+    */
+    public $data = null;
+
+    /**
+    * @var null|array $result;
+    */
+    public $result = null;
+ 
+}
