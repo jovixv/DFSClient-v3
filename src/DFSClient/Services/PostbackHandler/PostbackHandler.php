a0155221750187c35438de62c10db558e9229848
anatoliy_petrov <anatoliy.petrov.mail@gmail.com>
2020-12-28 11:35:57 +0200

    added postBackHandler

---
+++ src/DFSClient/Services/PostbackHandler/PostbackHandler.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace DFSClientV3\Services\PostbackHandler;
+
+
+use DFSClientV3\Models\AbstractModel;
+use DFSClientV3\Models\DataMapper;
+use DFSClientV3\Models\PaveDataOptions;
+
+class PostbackHandler
+{
+    protected $model;
+
+    public function __construct(AbstractModel $model)
+    {
+        $this->model = $model;
+    }
+
+    public function mapPostbackPayload ($postbackPayload)
+    {
+        $calledClass = $this->model->getCalledClass();
+        $status = true;
+        $pathToMainData = $this->model->getPathToMainData();
+
+        $isJsonContainVariadicType = $this->model->isJsonContainVariadicType();
+        $pathsToVariadicTypesAndValue  = $this->model->getPathsToVariadicTypesAndValue();
+        $customFunctionForPaths = $this->model->getCustomFunctionForPaths();
+
+        $mapper = new DataMapper($calledClass, $status, $pathToMainData);
+
+        $paveDataOptions = new PaveDataOptions();
+        $paveDataOptions->setJson($postbackPayload);
+        $paveDataOptions->setJsonContainVariadicType($isJsonContainVariadicType);
+        $paveDataOptions->setPathsToVariadicTypesAndValue($pathsToVariadicTypesAndValue);
+        $paveDataOptions->setCustomFunctionForPaths($customFunctionForPaths);
+
+        $mappedModel = $mapper->paveDataNew($paveDataOptions);
+
+        return $mappedModel;
+    }
+}
