bc7ae45a5809ee65bc1b40065c4a37273fa7bf9d
Vadym Borovok <vadym.borovok@gmail.com>
2021-01-26 14:48:26 +0200

    keyword_performance/locations_and_languages

---
+++ src/DFSClient/Entity/Custom/KeywordPerformanceGetLocationsAndLanguagesEntityMainTasksResult.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+use DFSClientV3\Entity\Custom\KeywordPerformanceGetLocationsAndLanguagesEntityMainTasksResultAvailable_locations;
+
+class KeywordPerformanceGetLocationsAndLanguagesEntityMainTasksResult 
+{    
+    /**
+    * @var null|string $language_name;
+    */
+    public $language_name = null;
+
+    /**
+    * @var null|string $language_code;
+    */
+    public $language_code = null;
+
+    /**
+    * @var array|KeywordPerformanceGetLocationsAndLanguagesEntityMainTasksResultAvailable_locations[] $available_locations;
+    */
+    public $available_locations = [];
+ 
+}
---
+++ src/DFSClient/Entity/Custom/KeywordPerformanceGetLocationsAndLanguagesEntityMainTasksResultAvailable_locations.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+class KeywordPerformanceGetLocationsAndLanguagesEntityMainTasksResultAvailable_locations 
+{    
+    /**
+    * @var null|integer $location_code;
+    */
+    public $location_code = null;
+
+    /**
+    * @var null|string $location_name;
+    */
+    public $location_name = null;
+
+    /**
+    * @var null|string $country_iso_code;
+    */
+    public $country_iso_code = null;
+
+    /**
+    * @var null|string $location_type;
+    */
+    public $location_type = null;
+ 
+}
