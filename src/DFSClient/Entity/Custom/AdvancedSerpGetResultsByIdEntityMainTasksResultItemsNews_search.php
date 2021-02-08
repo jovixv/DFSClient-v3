a5844bd00bb54be15239aace412e9446916c1592
jovix <vitaliy.lyabakh@outlook.com>
2021-02-08 17:12:02 +0300

    new entity for advanced types

---
+++ src/DFSClient/Entity/Custom/AdvancedSerpGetResultsByIdEntityMainTasksResultItemsNews_search.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+use DFSClientV3\Entity\Custom\AdvancedSerpGetResultsByIdEntityMainTasksResultItemsNews_searchRectangle;
+
+class AdvancedSerpGetResultsByIdEntityMainTasksResultItemsNews_search 
+{    
+    /**
+    * @var null|string $type;
+    */
+    public $type = null;
+
+    /**
+    * @var null|integer $rank_group;
+    */
+    public $rank_group = null;
+
+    /**
+    * @var null|integer $rank_absolute;
+    */
+    public $rank_absolute = null;
+
+    /**
+    * @var null|string $xpath;
+    */
+    public $xpath = null;
+
+    /**
+    * @var null|string $domain;
+    */
+    public $domain = null;
+
+    /**
+    * @var null|string $title;
+    */
+    public $title = null;
+
+    /**
+    * @var null|string $url;
+    */
+    public $url = null;
+
+    /**
+    * @var null|NULL $image_url;
+    */
+    public $image_url = null;
+
+    /**
+    * @var null|string $snippet;
+    */
+    public $snippet = null;
+
+    /**
+    * @var null|string $time_published;
+    */
+    public $time_published = null;
+
+    /**
+    * @var null|string $timestamp;
+    */
+    public $timestamp = null;
+
+    /**
+    * @var null|AdvancedSerpGetResultsByIdEntityMainTasksResultItemsNews_searchRectangle $rectangle;
+    */
+    public $rectangle = null;
+ 
+}
