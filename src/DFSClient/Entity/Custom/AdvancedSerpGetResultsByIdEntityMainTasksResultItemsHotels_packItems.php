820217a1b4b327345d9020cc27b7dd6774fd5d3c
jovix <vitaliy.lyabakh@outlook.com>
2021-02-26 16:50:03 +0300

    added hotel pack for ADVANCED GOOGLE SERP

---
+++ src/DFSClient/Entity/Custom/AdvancedSerpGetResultsByIdEntityMainTasksResultItemsHotels_packItems.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+use DFSClientV3\Entity\Custom\AdvancedSerpGetResultsByIdEntityMainTasksResultItemsHotels_packItemsRating;
+
+class AdvancedSerpGetResultsByIdEntityMainTasksResultItemsHotels_packItems 
+{    
+    /**
+    * @var null|string $type;
+    */
+    public $type = null;
+
+    /**
+    * @var null|string $price;
+    */
+    public $price = null;
+
+    /**
+    * @var null|string $title;
+    */
+    public $title = null;
+
+    /**
+    * @var null|string $description;
+    */
+    public $description = null;
+
+    /**
+    * @var null|string $domain;
+    */
+    public $domain = null;
+
+    /**
+    * @var null|string $url;
+    */
+    public $url = null;
+
+    /**
+    * @var null|boolean $is_paid;
+    */
+    public $is_paid = null;
+
+    /**
+    * @var null|AdvancedSerpGetResultsByIdEntityMainTasksResultItemsHotels_packItemsRating $rating;
+    */
+    public $rating = null;
+ 
+}
