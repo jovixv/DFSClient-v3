62f52aaa767bca0adaef871eab39ecaba76caabe
anatoliy_petrov <anatoliy.petrov.mail@gmail.com>
2021-04-16 14:59:34 +0300

    added new entities to AdvancedSerpGetResultsByIdEntityMainTasksResult

---
+++ src/DFSClient/Entity/Custom/AdvancedSerpGetResultsByIdEntityMainTasksResultItemsCommercial_units.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+use DFSClientV3\Entity\Custom\AdvancedSerpGetResultsByIdEntityMainTasksResultItemsCommercial_unitsRectangle;
+use DFSClientV3\Entity\Custom\AdvancedSerpGetResultsByIdEntityMainTasksResultItemsCommercial_unitsItems;
+
+
+class AdvancedSerpGetResultsByIdEntityMainTasksResultItemsCommercial_units
+{    
+    /**
+    * @var null|string $type;
+    */
+    public $type = null;
+
+    /**
+    * @var null|integer $rank_group;
+    */
+    public $rank_group = null;
+
+    /**
+    * @var null|integer $rank_absolute;
+    */
+    public $rank_absolute = null;
+
+    /**
+    * @var null|string $position;
+    */
+    public $position = null;
+
+    /**
+    * @var array|AdvancedSerpGetResultsByIdEntityMainTasksResultItemsCommercial_unitsItems[] $items;
+    */
+    public $items = [];
+
+    /**
+    * @var null|AdvancedSerpGetResultsByIdEntityMainTasksResultItemsCommercial_unitsRectangle $rectangle;
+    */
+    public $rectangle = null;
+ 
+}
