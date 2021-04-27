00e98501328f9f908b92da6aecce59855eda361f
anatoliy_petrov <anatoliy.petrov.mail@gmail.com>
2021-04-27 14:50:36 +0300

    added opPage spell entity

---
+++ src/DFSClient/Entity/Custom/OnPageGetPagesEntityMainTasksResultItemsHtmlMetaSpellMisspelled.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+class OnPageGetPagesEntityMainTasksResultItemsHtmlMetaSpellMisspelled
+{    
+    /**
+    * @var null|string $word;
+    */
+    public $word = null;
+ 
+}
