d66313b1543013cbd6fe22f75dfad8fe89897778
anatoliy_petrov <anatoliy.petrov.mail@gmail.com>
2021-08-06 14:57:53 +0300

    added new properties to OnPageForceStopEntityMainTasksData

--- src/DFSClient/Entity/Custom/OnPageForceStopEntityMainTasksData.php
+++ src/DFSClient/Entity/Custom/OnPageForceStopEntityMainTasksData.php
@@ -14,5 +14,20 @@ class OnPageForceStopEntityMainTasksData
     * @var null|string $function;
     */
     public $function = null;
+
+    /**
+     * @var null|string $target;
+     */
+    public $target = null;
+
+    /**
+     * @var null|integer $max_crawl_pages;
+     */
+    public $max_crawl_pages = null;
+
+    /**
+     * @var null|integer $crawl_delay;
+     */
+    public $crawl_delay = null;
  
 }
