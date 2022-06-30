329ed4f14afeffcae8e56820f837c42e0468eff1
Vadym Borovok <vadym.borovok@gmail.com>
2022-06-30 10:11:01 +0300

    serp screenshot

---
+++ src/DFSClient/Entity/Custom/SerpScreenshotEntityMainTasksData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+class SerpScreenshotEntityMainTasksData 
+{    
+    /**
+    * @var null|string $api;
+    */
+    public $api = null;
+
+    /**
+    * @var null|string $function;
+    */
+    public $function = null;
+
+    /**
+    * @var null|string $task_id;
+    */
+    public $task_id = null;
+ 
+}
