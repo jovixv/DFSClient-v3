436b20cb000bed4d58c10f019f0a8c75982df5f2
Kevin Fairbanks <kevin@kevinfairbanks.com>
2022-08-26 03:03:53 -0400

    GoogleTrendsGetResultsByID - Add Top & Rising

--- src/DFSClient/Entity/Custom/GoogleTrendsGetResultsByIdEntityMainTasksResultItemsData.php
+++ src/DFSClient/Entity/Custom/GoogleTrendsGetResultsByIdEntityMainTasksResultItemsData.php
@@ -5,24 +5,6 @@ namespace DFSClientV3\Entity\Custom;
 
 class GoogleTrendsGetResultsByIdEntityMainTasksResultItemsData 
 {    
-    /**
-    * @var null|string $date_from;
-    */
-    public $date_from = null;
-
-    /**
-    * @var null|string $date_to;
-    */
-    public $date_to = null;
-
-    /**
-    * @var null|integer $timestamp;
-    */
-    public $timestamp = null;
-
-    /**
-    * @var null|array $values;
-    */
-    public $values = null;
+    
  
 }
---
+++ src/DFSClient/Entity/Custom/GoogleTrendsGetResultsByIdEntityMainTasksResultItemsDataTop.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+class GoogleTrendsGetResultsByIdEntityMainTasksResultItemsDataTop 
+{    
+    
+    /**
+    * @var null|string $query;
+    */
+    public $query = null;
+
+    /**
+    * @var null|integer $value;
+    */
+    public $value = null;
+ 
+}
