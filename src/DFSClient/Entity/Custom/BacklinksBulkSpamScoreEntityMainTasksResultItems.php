3842296c96a24cdf843f2c30a9132f5cd399c0cd
Vadym Borovok <vadym.borovok@gmail.com>
2022-09-07 14:48:35 +0300

    add backlinks/bulk_spam_score

---
+++ src/DFSClient/Entity/Custom/BacklinksBulkSpamScoreEntityMainTasksResultItems.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+class BacklinksBulkSpamScoreEntityMainTasksResultItems 
+{    
+    /**
+    * @var null|string $type;
+    */
+    public $type = null;
+
+    /**
+    * @var null|string $target;
+    */
+    public $target = null;
+
+    /**
+    * @var null|integer $spam_score;
+    */
+    public $spam_score = null;
+ 
+}
