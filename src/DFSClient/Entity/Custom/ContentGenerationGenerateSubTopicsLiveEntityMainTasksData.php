1a4323b8795285ecae32bc1bda341a0d9f1b1282
Danil <work.tivikov@gmail.com>
2023-03-06 16:05:48 +0100

    add sub_topics endpoint to content generation api

---
+++ src/DFSClient/Entity/Custom/ContentGenerationGenerateSubTopicsLiveEntityMainTasksData.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+class ContentGenerationGenerateSubTopicsLiveEntityMainTasksData 
+{    
+    /**
+    * @var null|string $api;
+    */
+    public $api = null;
+
+    /**
+    * @var null|string $function;
+    */
+    public $function = null;
+
+    /**
+    * @var null|string $topic;
+    */
+    public $topic = null;
+
+    /**
+    * @var null|double $creativity_index;
+    */
+    public $creativity_index = null;
+ 
+}
