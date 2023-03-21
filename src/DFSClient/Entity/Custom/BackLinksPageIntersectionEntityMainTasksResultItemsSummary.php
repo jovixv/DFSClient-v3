fa2b86d2c17c4561ed7f9e34704c90951da3712f
Danil <work.tivikov@gmail.com>
2023-03-21 13:16:44 +0100

    backlinks/page_intersection: changing entities for new structure

---
+++ src/DFSClient/Entity/Custom/BackLinksPageIntersectionEntityMainTasksResultItemsSummary.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+class BackLinksPageIntersectionEntityMainTasksResultItemsSummary 
+{    
+    /**
+    * @var null|integer $intersections_count;
+    */
+    public $intersections_count = null;
+ 
+}
