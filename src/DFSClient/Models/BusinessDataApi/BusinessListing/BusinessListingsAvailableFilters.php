50dc56820b3eb51765bef49b09b3b8aa8ad25acc
Vadym Borovok <vadym.borovok@gmail.com>
2023-04-26 18:54:45 +0300

    add BusinessListingAPI

---
+++ src/DFSClient/Models/BusinessDataApi/BusinessListing/BusinessListingsAvailableFilters.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DFSClientV3\Models\BusinessDataApi\BusinessListing;
+
+use DFSClientV3\Entity\Custom\BusinessListingsAvailableFiltersEntityMain;
+use DFSClientV3\Models\AbstractModel;
+
+class BusinessListingsAvailableFilters extends AbstractModel {
+	protected $method = 'GET';
+	protected $isSupportedMerge = false;
+	protected $pathToMainData = 'tasks->{$postID}->result';
+	protected $requestToFunction = 'business_data/business_listings/available_filters';
+	protected $resultShouldBeTransformedToArray = true;
+
+	/**
+	 * @return BusinessListingsAvailableFiltersEntityMain
+	 */
+	public function get(): BusinessListingsAvailableFiltersEntityMain {
+		return parent::get();
+	}
+}
