2051aaa7282e2aefc411ffdb8bf84f920f6594e4
sergeyskovorodkin <skovorodkinsergey86@gmail.com>
2023-05-01 10:21:04 +0300

    add onpage/raw_html

---
+++ src/DFSClient/Entity/Custom/OnPageRawHtmlEntityMainTasksResultItems.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+class OnPageRawHtmlEntityMainTasksResultItems 
+{    
+    /**
+    * @var null|string $html;
+    */
+    public $html = null;
+ 
+}
