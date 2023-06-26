57aa62daeada33da1c1b81dfd4b1356399ca6a0f
Danil <work.tivikov@gmail.com>
2023-06-26 22:52:11 +0200

    add TechnologiesTechnologyStatsLive model

---
+++ src/DFSClient/Entity/Custom/TechnologiesTechnologyStatsLiveEntityMainTasksResult.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+class TechnologiesTechnologyStatsLiveEntityMainTasksResult 
+{    
+    /**
+    * @var null|string $technology;
+    */
+    public $technology = null;
+
+    /**
+    * @var null|string $date_from;
+    */
+    public $date_from = null;
+
+    /**
+    * @var null|string $date_to;
+    */
+    public $date_to = null;
+
+    /**
+    * @var null|integer $items_count;
+    */
+    public $items_count = null;
+
+    /**
+    * @var array|TechnologiesTechnologyStatsLiveEntityMainTasksResultItems[] $items;
+    */
+    public $items = [];
+ 
+}
---
+++ src/DFSClient/Entity/Custom/TechnologiesTechnologyStatsLiveEntityMainTasksResultItems.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+class TechnologiesTechnologyStatsLiveEntityMainTasksResultItems 
+{    
+    /**
+    * @var null|string $type;
+    */
+    public $type = null;
+
+    /**
+    * @var null|string $date;
+    */
+    public $date = null;
+
+    /**
+    * @var null|integer $domains_count;
+    */
+    public $domains_count = null;
+
+    /**
+    * @var null|array $countries;
+    */
+    public $countries = null;
+
+    /**
+    * @var null|array $languages;
+    */
+    public $languages = null;
+
+    /**
+    * @var null|array $domains_rank;
+    */
+    public $domains_rank = null;
+ 
+}
