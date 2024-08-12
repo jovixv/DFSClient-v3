041ecb8d7ff111f3c49d6852bfe60a15f4c4e448
Vadym Borovok <vadym.borovok@gmail.com>
2024-08-12 15:11:23 +0300

    add keywords_data/clickstream_data/bulk_search_volume

---
+++ src/DFSClient/Entity/Custom/ClickstreamBulkSearchVolumeLiveEntityMainTasksResult.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+class ClickstreamBulkSearchVolumeLiveEntityMainTasksResult 
+{    
+    /**
+    * @var null|integer $location_code;
+    */
+    public $location_code = null;
+
+    /**
+    * @var null|integer $items_count;
+    */
+    public $items_count = null;
+
+    /**
+    * @var array|ClickstreamBulkSearchVolumeLiveEntityMainTasksResultItems[] $items;
+    */
+    public $items = [];
+ 
+}
---
+++ src/DFSClient/Entity/Custom/ClickstreamBulkSearchVolumeLiveEntityMainTasksResultItems.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+use DFSClientV3\Entity\Custom\ClickstreamBulkSearchVolumeLiveEntityMainTasksResultItemsMonthly_searches;
+
+class ClickstreamBulkSearchVolumeLiveEntityMainTasksResultItems 
+{    
+    /**
+    * @var null|string $keyword;
+    */
+    public $keyword = null;
+
+    /**
+    * @var null|integer $search_volume;
+    */
+    public $search_volume = null;
+
+    /**
+    * @var array|ClickstreamBulkSearchVolumeLiveEntityMainTasksResultItemsMonthly_searches[] $monthly_searches;
+    */
+    public $monthly_searches = [];
+ 
+}
