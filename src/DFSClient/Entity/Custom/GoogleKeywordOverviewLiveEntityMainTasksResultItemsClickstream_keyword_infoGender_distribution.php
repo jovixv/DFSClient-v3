09f99d0fd07a47a6a7abb7d1dada8e559750c14e
Danylo Tivikov <danylo.tivikov@softoria.com>
2025-03-25 15:34:40 +0100

    fix missing properties for dataforseo_labs/google/keyword_overview

---
+++ src/DFSClient/Entity/Custom/GoogleKeywordOverviewLiveEntityMainTasksResultItemsClickstream_keyword_infoGender_distribution.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+#[\AllowDynamicProperties]
+class GoogleKeywordOverviewLiveEntityMainTasksResultItemsClickstream_keyword_infoGender_distribution 
+{    
+    /**
+    * @var null|integer $female;
+    */
+    public $female = null;
+
+    /**
+    * @var null|integer $male;
+    */
+    public $male = null;
+ 
+}
---
+++ src/DFSClient/Entity/Custom/GoogleKeywordOverviewLiveEntityMainTasksResultItemsKeyword_info_normalized_with_bing.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+use DFSClientV3\Entity\Custom\GoogleKeywordOverviewLiveEntityMainTasksResultItemsKeyword_info_normalized_with_bingMonthly_searches;
+
+#[\AllowDynamicProperties]
+class GoogleKeywordOverviewLiveEntityMainTasksResultItemsKeyword_info_normalized_with_bing 
+{    
+    /**
+    * @var null|string $last_updated_time;
+    */
+    public $last_updated_time = null;
+
+    /**
+    * @var null|integer $search_volume;
+    */
+    public $search_volume = null;
+
+    /**
+    * @var null|boolean $is_normalized;
+    */
+    public $is_normalized = null;
+
+    /**
+    * @var array|GoogleKeywordOverviewLiveEntityMainTasksResultItemsKeyword_info_normalized_with_bingMonthly_searches[] $monthly_searches;
+    */
+    public $monthly_searches = [];
+ 
+}
