18bd9e316bc1a81123ecf93c8e8cb04befe074cf
Filippo Toso <filippo.toso@creativepark.it>
2025-06-08 07:13:14 +0200

    feat: add Tripadvisor ReviewsGetCompletedTasks model, fix TripadvisorReviewsGetResultsByTaskId

---
+++ src/DFSClient/Models/BusinessDataApi/Tripadvisor/Reviews/ReviewsGetCompletedTasks.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace DFSClientV3\Models\BusinessDataApi\Tripadvisor\Reviews;
+
+use DFSClientV3\Models\AbstractModel;
+
+class ReviewsGetCompletedTasks extends AbstractModel
+{
+    protected $method = 'GET';
+    protected $isSupportedMerge = false;
+    protected $pathToMainData = 'tasks->{$postID}->result';
+    protected $requestToFunction = 'business_data/tripadvisor/reviews/tasks_ready';
+    protected $resultShouldBeTransformedToArray = true;
+
+    /**
+     * @deprecated This method is not necessary, only 'google'.
+     * @param string $seName
+     * @return $this
+     */
+    public function setSe(string $seName)
+    {
+        $this->requestToFunction = str_replace('{$se}', $seName, $this->requestToFunction);
+        return $this;
+    }
+
+    /**
+     * @return \DFSClientV3\Entity\Custom\ReviewsGetCompletedTasksEntityMain
+     */
+    public function get(): \DFSClientV3\Entity\Custom\ReviewsGetCompletedTasksEntityMain
+    {
+        return parent::get();
+    }
+}
