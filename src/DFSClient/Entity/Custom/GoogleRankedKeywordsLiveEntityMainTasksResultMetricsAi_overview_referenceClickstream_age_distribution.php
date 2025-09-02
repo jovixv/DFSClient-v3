2c5b169d22282641af7a0b6976f45ecba823ee63
Danylo Tivikov <danylo.tivikov@softoria.com>
2025-09-02 13:12:25 +0200

    support ai_overview_reference response

---
+++ src/DFSClient/Entity/Custom/GoogleRankedKeywordsLiveEntityMainTasksResultMetricsAi_overview_referenceClickstream_age_distribution.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DFSClientV3\Entity\Custom;
+
+
+#[\AllowDynamicProperties]
+class GoogleRankedKeywordsLiveEntityMainTasksResultMetricsAi_overview_referenceClickstream_age_distribution
+{    
+
+}
