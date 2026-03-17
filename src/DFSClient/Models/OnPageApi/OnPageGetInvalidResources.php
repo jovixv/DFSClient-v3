788a26cb0924cd7cfc3865df0f0bc5ae6f5bc423
Sergei Yurchenko <sergyurch@gmail.com>
2026-03-17 11:50:29 +0200

    Add on_page/invalid_resources

---
+++ src/DFSClient/Models/OnPageApi/OnPageGetInvalidResources.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace DFSClientV3\Models\OnPageApi;
+
+use DFSClientV3\Models\AbstractModel;
+
+class OnPageGetInvalidResources extends AbstractModel {
+	protected $method = 'POST';
+	protected $isSupportedMerge = false;
+	protected $pathToMainData = 'tasks->0->result';
+	protected $requestToFunction = 'on_page/invalid_resources';
+	protected $resultShouldBeTransformedToArray = true;
+	protected $useNewMapper = true;
+
+	/**
+	 * @param string $taskID
+	 * @return $this
+	 */
+	public function setTaskId(string $taskID) {
+		$this->payload['id'] = $taskID;
+		return $this;
+	}
+
+	/**
+	 * @param int $limit
+	 * @return $this
+	 */
+	public function setLimit(int $limit) {
+		$this->payload['limit'] = $limit;
+		return $this;
+	}
+
+	/**
+	 * @param int $offset
+	 * @return $this
+	 */
+	public function setOffset(int $offset) {
+		$this->payload['offset'] = $offset;
+		return $this;
+	}
+
+	/**
+	 * @param array $orderBy
+	 * @return $this
+	 */
+	public function setOrderBy(array $orderBy) {
+		$this->payload['order_by'] = $orderBy;
+		return $this;
+	}
+
+	/**
+	 * @param array $filter
+	 * @return $this
+	 */
+	public function setFilter(array $filter) {
+		$this->payload['filters'] = $filter;
+		return $this;
+	}
+}
